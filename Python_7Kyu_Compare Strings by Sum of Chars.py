b8fad6c416a50c50fa5017301caec33326779cda
nicesixman <nicesixman@naver.com>
2018-11-29 22:07:42 +0900

    Python_7Kyu_Compare Strings by Sum of Chars.py
    https://www.codewars.com/kata/576bb3c4b1abc497ec000065/train/python
    조건문을 꽤 난잡하게 쓴 느낌이 있긴하지만, 모든 예외사항을 일일이 체크하는 덕에 이상 없이 잘 돌아간다.

--- Python_7Kyu_Compare Strings by Sum of Chars.py
+++ Python_7Kyu_Compare Strings by Sum of Chars.py
@@ -1,18 +1,25 @@
 def compare(s1, s2):
     s1sum = 0
     s2sum = 0
-    if s1 is None or s1.isalpha() == 0:
+    if s1 is None:
         s1sum = 0
-    if s2 is None or s2.isalpha() == 0:
-        s2sum = 0
     else:
         for chrsearch in range(0, len(s1)):
-            if s1[chrsearch].isupper() == 1:
+            if s1[chrsearch].isalpha() == 0:
+                s1sum = 0
+                break
+            elif s1[chrsearch].isupper() == 1:
                 s1sum += (int(ord(s1[chrsearch])))
             elif s1[chrsearch].isupper() == 0:
                 s1sum += (int(ord(s1[chrsearch])))-32
+    if s2 is None:
+        s2sum = 0
+    else:
         for chrsearch in range(0, len(s2)):
-            if s2[chrsearch].isupper() == 1:
+            if s2[chrsearch].isalpha() == 0:
+                s2sum = 0
+                break
+            elif s2[chrsearch].isupper() == 1:
                 s2sum += (int(ord(s2[chrsearch])))
             elif s2[chrsearch].isupper() == 0:
                 s2sum += (int(ord(s2[chrsearch])))-32
