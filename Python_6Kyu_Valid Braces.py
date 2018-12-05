03c0c4e604f9bd5deec309631a55a22143185b2d
nicesixman <nicesixman@naver.com>
2018-12-05 23:44:58 +0900

    Python_6Kyu_Valid Braces.py
    https://www.codewars.com/kata/5277c8a221e209d3f6000b56/train/python
    print('need for algorithm') 부분에 본격적으로 알고리즘을 구상하면 된다.

---
+++ Python_6Kyu_Valid Braces.py
@@ -0,0 +1,32 @@
+def validBraces(string):
+    new_list = []
+    for str_length in range(0, len(string)):
+        if string[str_length] == '(':
+            new_list.append(1)
+            continue
+        elif string[str_length] == ')':
+            new_list.append(-1)
+            continue
+        elif string[str_length] == '{':
+            new_list.append(2)
+            continue
+        elif string[str_length] == '}':
+            new_list.append(-2)
+            continue
+        elif string[str_length] == '[':
+            new_list.append(3)
+            continue
+        elif string[str_length] == ']':
+            new_list.append(-3)
+            continue
+    print(new_list)
+    if len(new_list) % 2 == 1:
+        return False
+    elif sum(new_list) == 0:
+        print('need for algorithm')
+        return True
+    else:
+        return False
+
+
+validBraces('(){}[]')
