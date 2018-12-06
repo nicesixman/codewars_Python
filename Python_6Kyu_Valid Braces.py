e8b3092342af9944acc56ec79530d2e69e7ca4f7
nicesixman <nicesixman@naver.com>
2018-12-06 14:40:18 +0900

    Python_6Kyu_Valid Braces.py
    https://www.codewars.com/kata/5277c8a221e209d3f6000b56/train/python
    예외 처리가 개별로 작성되어 있긴 해도, 이상 없이 작동된다. 제출 완료!

--- Python_6Kyu_Valid Braces.py
+++ Python_6Kyu_Valid Braces.py
@@ -1,33 +1,40 @@
 def validBraces(string):
     new_list = []
+    if string[0] == ')' or string[0] == '}' or string[0] == ']':
+        return False
     for str_length in range(0, len(string)):
         if string[str_length] == '(':
             new_list.append('(')
-            print(new_list)
-            continue
+            stack = 1
+            # print(new_list)
         elif string[str_length] == '{':
             new_list.append('{')
-            print(new_list)
-            continue
+            stack = 1
+            # print(new_list)
         elif string[str_length] == '[':
             new_list.append('[')
-            print(new_list)
-            continue
-        elif string[str_length-1] == '(' and string[str_length] == ')':
+            stack = 1
+            # print(new_list)
+        elif string[str_length] == ')' and string[str_length - stack] == '(':
             new_list.pop()
-            print(new_list)
-            continue
-        elif string[str_length-1] == '{' and string[str_length] == '}':
+            stack += 2
+            # print(new_list)
+            # print(stack)
+        elif string[str_length] == '}' and string[str_length - stack] == '{':
             new_list.pop()
-            print(new_list)
-            continue
-        elif string[str_length-1] == '[' and string[str_length] == ']':
+            stack += 2
+            # print(new_list)
+            # print(stack)
+        elif string[str_length] == ']' and string[str_length - stack] == '[':
             new_list.pop()
-            print(new_list)
-            continue
+            stack += 2
+            # print(new_list)
+            # print(stack)
         else:
             return False
+    if new_list:
+        return False
     return True
 
 
-validBraces('(({{[[]]}}))')
+validBraces('([{}])')
