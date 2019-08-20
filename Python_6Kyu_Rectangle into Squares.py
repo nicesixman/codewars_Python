9fcc546c6b838dce30bb0dcd45e85c56c144318f
nicesixman <nicesixman@naver.com>
2019-08-20 23:43:32 +0900

    Python_6Kyu_Rectangle into Squares.py
    https://www.codewars.com/kata/rectangle-into-squares/train/python
    문제에 대해 감 잡은 후 알고리즘 구상하여 풀어냄. (최종 제출 완료)
    상당히 빠른 시간 안에 Best Practices / Clever Solution과 흡사하게 풀어냈다는 점에 큰 의미가 있는듯...!!

--- Python_6Kyu_Rectangle into Squares.py
+++ Python_6Kyu_Rectangle into Squares.py
@@ -1,5 +1,27 @@
 def sqInRect(lng, wdth):
-    print("Hello World")
-    return None
+    lng_temp = lng
+    wdth_temp = wdth
+    squares = []
 
-sqInRect(5, 5)
+    if lng is wdth:
+        return None
+    else:
+        while lng_temp and wdth_temp is not 0:
+            if lng_temp > wdth_temp:
+                squares.append(wdth_temp)
+                lng_temp -= wdth_temp
+            elif lng_temp < wdth_temp:
+                squares.append(lng_temp)
+                wdth_temp -= lng_temp
+            elif wdth_temp is lng_temp:
+                squares.append(wdth_temp)
+                lng_temp = 0
+                wdth_temp = 0
+            else:
+                print('Error!')
+                break
+            print(squares)                              # for debug
+            print(lng_temp, wdth_temp)      # for debug
+        return squares
+
+sqInRect(5, 3)
